e0aed6d6fa9d084f53f8709a5abd7ee09178ea92
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-10-28 17:58:12 -0400

    Created a handling POST request

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ const app = express();
 //Middleware
 app.use(express.json());
 
+//Handling POST Requests
+app.post('/api/users', (req, res) => {
+  const user  = req.body;
+
+  if (!user.name || !user.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' });
+  } else {
+    data.insert(user)
+      .then(user => {
+        res.status(201).json({ message: 'the user was added.' });
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage:
+            'There was an error while saving the user to the database',
+        });
+      });
+  }
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`))
