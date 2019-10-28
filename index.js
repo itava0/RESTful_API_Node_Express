828c71d6b998f834f9dc9994382c198513747665
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-10-28 17:55:58 -0400

    Listening server on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// implement your API here
+const express = require('express');
+const data = require('./data/db.js')
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`))
